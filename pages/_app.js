95fe674fdbd7cda3512912804c44fa3310979057
Joshua Stevens-Stein <jstevensstein@gmail.com>
2024-09-08 22:28:55 -0400

    Adding navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import "./_app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+function MyApp({Component, pageProps}) {
+  return <>
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Welcome</Navbar.Brand>
+        <Navbar.Collapse>
+          <Nav.Link href="/pallete">Add Pallete</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Container><Component {...pageProps} /></Container>
+  </>;
 }
 
 export default MyApp;
