31577da42b2605d63a78adf2027349503dd01306
Joshua Stevens-Stein <jstevensstein@gmail.com>
2024-09-10 20:57:24 -0400

    - Adding ability to search for palettes based on their name

--- components/search/index.js
+++ components/search/index.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { searchPalettes } from "../../services/paletteService";
 import { ListGroup, Stack } from "react-bootstrap";
 import { isColorDark } from "../../utils";
+import Form from "react-bootstrap/Form";
+import debounce from "lodash/debounce";
 
 const Search = () => {
     const [query, setQuery] = useState("");
@@ -10,13 +12,27 @@ const Search = () => {
 
     const getPalettes = async () => {
       try {
-        const result = await searchPalettes(name);
+        const result = await searchPalettes(query);
         setPalettes(result);
       } catch (error) {
         setError(error.message);
       }
     }
 
+
+    const debouncedSearch = debounce((q) => {
+      getPalettes();
+    }, 500);
+
+    useEffect(() => {
+      debouncedSearch(query);
+
+      return () => debouncedSearch.cancel();
+    }, [query])
+
+    /*
+     Initialize with a primary set of queries;
+     */
     useEffect(() => {
       getPalettes();
     }, [setPalettes, axios]);
@@ -24,25 +40,32 @@ const Search = () => {
 
     return (
       <>
+        <Form.Control
+          value={query}
+          onChange={(ev) => {
+            setQuery(ev.target.value);
+          }}
+          placeholder="Search..."
+        ></Form.Control>
         {palettes ?
           <ListGroup>
             {palettes.map((palette, i) => (
               <ListGroup.Item key={i}>
-                <a href={`/palette/${palette.id}`} style={{textDecoration:"none"}}>
-                <h3>{palette.name}</h3>
-                <Stack direction="horizontal" gap="4">
-                  {palette.colors.map((color, j) => {
-                    const code = `#${color}`;
-                    return (
-                      <div style={{backgroundColor: code}} key={j}
-                           className={isColorDark(color) ? "text-light" : "text-dark"}>
-                        <h4>
-                          {code}
-                        </h4>
-                      </div>
-                    )
-                  })}
-                </Stack>
+                <a href={`/palette/${palette.id}`} style={{textDecoration: "none"}}>
+                  <h3>{palette.name}</h3>
+                  <Stack direction="horizontal" gap="4">
+                    {palette.colors.map((color, j) => {
+                      const code = `#${color}`;
+                      return (
+                        <div style={{backgroundColor: code}} key={j}
+                             className={isColorDark(color) ? "text-light" : "text-dark"}>
+                          <h4>
+                            {code}
+                          </h4>
+                        </div>
+                      )
+                    })}
+                  </Stack>
                 </a>
               </ListGroup.Item>
             ))}
